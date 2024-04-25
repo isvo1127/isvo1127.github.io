d70e8652320911874c8a81b392705364ba101fb9
Isak Voxlin <isvo1127@storsjogymnasiet.se>
2024-04-25 09:11:38 +0200

    m

---
+++ gammal sida/javscript/script.js
@@ -0,0 +1,15 @@
+console.log("välkommen")
+ 
+let namn = "asdas"
+ 
+function setText() {
+    document.getElementById("text").innerHTML = document.getElementById("input").value
+  
+   let r = Math.random() *255
+   let g = Math.random() *255
+   let b = Math.random() *255
+   document.body.style.backgroundColor = `rgb(${r},${g},${b})`
+    Math.floor()
+}
+ 
+//document.body.style.backgroundcolor = `rgb(${100},${255},${0})` ;
